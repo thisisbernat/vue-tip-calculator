60196ebde8bea4e88b1f0f5ea66d9fb2b289e44c
Bernat Escarra <bernat.escarra@holavilma.com>
2023-04-16 20:38:11 +0200

    Update Tailwind theme & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      cyan: {
+        DEFAULT: 'hsl(172, 67%, 45%)',
+        dark: 'hsl(183, 100%, 15%)'
+      },
+      'grayish-cyan': {
+        dark: 'hsl(186, 14%, 43%)',
+        DEFAULT: 'hsl(184, 14%, 56%)',
+        light: 'hsl(185, 41%, 84%)',
+        extralight: 'hsl(189, 41%, 97%)',
+      },
+    },
+    fontFamily: {
+      sans: ['Space Mono', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
